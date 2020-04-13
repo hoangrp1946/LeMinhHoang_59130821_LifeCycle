038d9dbb4876a643d3bd260bfedaa385ad29daeb
hoangrp1946/LeMinhHoang_59130821_LifeCycle <hoangrp1946>
2020-04-13 16:43:11 +0700

    v1.0

---
+++ app/src/main/java/com/example/leminhhoang_59130821_lifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.leminhhoang_59130821_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
